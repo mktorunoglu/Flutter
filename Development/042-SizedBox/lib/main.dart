ba49a5d562e73cbd230bf0792346260d87cd8615
Mehmet Kamil Torunoğlu <32582511+mktorunoglu@users.noreply.github.com>
2021-07-18 22:39:52 +0300

    Flutter

---
+++ Development/042-SizedBox/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Basic Flutter Layout Concepts',
+      home: MyWidget(),
+    ),
+  );
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        BlueBox(),
+        SizedBox(
+          width: 100,
+          height: 100,
+        ),
+        BlueBox(),
+        /*
+        SizedBox(
+          width: 200,
+          height: 200,
+          child: BlueBox(),
+        ),
+        */
+        BlueBox(),
+      ],
+    );
+  }
+}
+
+class BlueBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        border: Border.all(),
+      ),
+    );
+  }
+}
