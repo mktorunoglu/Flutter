781de1a48e68fbacb1d2766f028d50cc24be3883
Mehmet Kamil Torunoğlu <32582511+mktorunoglu@users.noreply.github.com>
2021-07-20 19:01:34 +0300

    Flutter

---
+++ Codelabs/004-HowToDebugLayoutIssuesWithTheFlutterInspector/lib/main.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Menu());
+}
+
+class MenuItem extends StatelessWidget {
+  const MenuItem(this.icon, this.itemText);
+  final String icon;
+  final String itemText;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Text(
+        icon,
+        style: TextStyle(
+          fontSize: 30.0,
+        ),
+      ),
+      title: Text(itemText),
+    );
+  }
+}
+
+class Menu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Menu Demo'),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Example1(),
+              Example2(),
+              Example3(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Example1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 30.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: Text(
+              'Explore the restaurant\'s delicious menu items below! Explore the restaurant\'s delicious menu items below!',
+              style: TextStyle(
+                fontSize: 18.0,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Example2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      // return Expanded(
+      child: ListView(
+        children: [
+          MenuItem('🍔', 'Burger'),
+          MenuItem('🌭', 'Hot Dog'),
+          MenuItem('🍟', 'Fries'),
+          MenuItem('🥤', 'Soda'),
+          MenuItem('🍦', 'Ice Cream'),
+        ],
+      ),
+    );
+  }
+}
+
+class Example3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            print('Pickup button pressed.');
+          },
+          child: Text(
+            'Pickup',
+          ),
+        ),
+        SizedBox(
+          height: 50,
+          child: VerticalDivider(
+            width: 20.0,
+            thickness: 5.0,
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            print('Delivery button pressed.');
+          },
+          child: Text(
+            'Delivery',
+          ),
+        )
+      ],
+    );
+  }
+}
