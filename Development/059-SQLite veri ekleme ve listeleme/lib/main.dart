2c20fe3332698dd1702717d558c36cdc7da03c50
Mehmet Kamil Torunoğlu <32582511+mktorunoglu@users.noreply.github.com>
2021-08-09 15:41:02 +0300

    Flutter

---
+++ Development/059-SQLite veri ekleme ve listeleme/lib/main.dart
@@ -0,0 +1,84 @@
+import 'dart:async';
+import 'package:flutter/widgets.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class Dog {
+  final int id;
+  final String name;
+  final int age;
+
+  Dog({
+    required this.id,
+    required this.name,
+    required this.age,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'age': age,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'Dog{id: $id, name: $name, age: $age}';
+  }
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final database = openDatabase(
+    join(await getDatabasesPath(), 'doggie_database.db'),
+    onCreate: (db, version) {
+      return db.execute(
+        'CREATE TABLE dogs(id INTEGER PRIMARY KEY, name TEXT, age INTEGER)',
+      );
+    },
+    version: 1,
+  );
+
+  Future<void> insertDog(Dog dog) async {
+    final db = await database;
+
+    await db.insert(
+      'dogs',
+      dog.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<List<Dog>> dogs() async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query('dogs');
+
+    return List.generate(
+      maps.length,
+      (i) {
+        return Dog(
+          id: maps[i]['id'],
+          name: maps[i]['name'],
+          age: maps[i]['age'],
+        );
+      },
+    );
+  }
+
+  var fido = Dog(
+    id: 0,
+    name: 'Fido',
+    age: 35,
+  );
+
+  var dogi = Dog(
+    id: 1,
+    name: 'Dogi',
+    age: 5,
+  );
+
+  await insertDog(fido);
+  await insertDog(dogi);
+  print(await dogs());
+}
