a714853acdf4315fe44650833f90cd596bc4e546
Mehmet Kamil Torunoğlu <32582511+mktorunoglu@users.noreply.github.com>
2021-08-12 16:07:28 +0300

    Flutter

---
+++ Development/095-Tween/lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Demo",
+      home: TweenDemo(),
+    );
+  }
+}
+
+class TweenDemo extends StatefulWidget {
+  const TweenDemo({Key? key}) : super(key: key);
+  static const String routeName = '/basics/tweens';
+
+  @override
+  _TweenDemoState createState() => _TweenDemoState();
+}
+
+class _TweenDemoState extends State<TweenDemo>
+    with SingleTickerProviderStateMixin {
+  static const Duration _duration = Duration(seconds: 1);
+  static const double accountBalance = 1000000;
+  late final AnimationController controller;
+  late final Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(vsync: this, duration: _duration)
+      ..addListener(() {
+        setState(() {});
+      });
+    animation = Tween(begin: 0.0, end: accountBalance).animate(controller);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tweens'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ConstrainedBox(
+              constraints: const BoxConstraints(maxWidth: 200),
+              child: Text('\$${animation.value.toStringAsFixed(2)}',
+                  style: const TextStyle(fontSize: 24)),
+            ),
+            ElevatedButton(
+              child: Text(
+                controller.status == AnimationStatus.completed
+                    ? 'Buy a Mansion'
+                    : 'Win Lottery',
+              ),
+              onPressed: () {
+                if (controller.status == AnimationStatus.completed) {
+                  controller.reverse();
+                } else {
+                  controller.forward();
+                }
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
